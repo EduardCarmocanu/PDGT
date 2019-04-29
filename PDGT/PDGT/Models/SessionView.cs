164ea97cb5358fbd69caff4e45e458750b815abf
Pia Roel Jorgensen <pia@designersbar.dk>
2019-04-29 10:06:46 +0200

    ADD: CarouselView

---
+++ PDGT/PDGT/Models/SessionView.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PDGT.Models
+{
+    class SessionView
+    {
+        public string SessionTitle { get; set; }
+        public string Sets { get; set; }
+        public string Reps { get; set; }
+        public string WeightInKg { get; set; }
+    }
+}
---
+++ PDGT/PDGT/Pages/SessionPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using PDGT.Models;
+
+namespace PDGT.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SessionPage : ContentPage
+	{
+        public SessionPage()
+        {
+            InitializeComponent();
+
+            var images = new List<string>
+           {
+              "Weight.png",
+              "Weight.png",
+              "Weight.png"
+           };
+
+
+            var views = new List<string>
+            {
+                "1",
+                "2",
+                "3"
+            };
+
+            CarouselViews.ItemsSource = views;
+        }
+
+
+            private void GuideBtn_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
