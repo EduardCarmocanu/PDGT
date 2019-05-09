2b59425934338c2cae8f61abcfeceae31726cd45
aSondergaard <andrewsondergaard@gmail.com>
2019-05-09 15:49:35 +0200

    Feature: all pages for prequestionnaire added with basic functionality

--- PDGT/PDGT/Pages/PreSessionPage.xaml.cs
+++ PDGT/PDGT/Pages/PreSessionPage.xaml.cs
@@ -16,8 +16,12 @@ namespace PDGT.Pages
 		public PreSessionPage ()
 		{
             PreQuestionnaire myQ = new PreQuestionnaire();
+            //Call responsive methods on pages with comments
+            SizeChanged += QuestionnaireCommentPage;
+            SizeChanged += QuestionnaireSymptombsCommentPage;
 
-			InitializeComponent ();
+
+            InitializeComponent ();
 		}
 
         private void TakenPainKillers_Toggled(object sender, ToggledEventArgs e)
@@ -25,10 +29,12 @@ namespace PDGT.Pages
             if(e.Value == true)
             {
                 PainkillerTakenResult.Text = "Yes";
+                PainkillerOptions.IsVisible = true;
             }
             else
             {
                 PainkillerTakenResult.Text = "No";
+                PainkillerOptions.IsVisible = false;
             }
         }
 
@@ -49,6 +55,18 @@ namespace PDGT.Pages
                 SymptombsDescription.BackgroundColor = Color.FromHex("#CCCCCC");
                 SymptombsDescription.Opacity = 0.5;
             }
+        }
+
+        //Make the Entry on the questionnaire "comment" page responsive according to the screenheight.
+        void QuestionnaireCommentPage(Object sender, EventArgs e)
+        {
+            QuestionnaireComment.HeightRequest = Math.Min(this.Height, 275);
+        }
+
+        //Make the Entry on the questionnaire "symptombs" page responsive according to the screenheight.
+        void QuestionnaireSymptombsCommentPage(Object sender, EventArgs e)
+        {
+            SymptombsDescription.HeightRequest = Math.Min(this.Height, 275);
         }
 
     }
