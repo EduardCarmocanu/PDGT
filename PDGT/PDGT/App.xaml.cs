46bf01f7d4187279b76f7d821ce2fc24b77e3330
aSondergaard <andrewsondergaard@gmail.com>
2019-05-09 22:54:17 +0200

    Fix: typo that made app crash is now fixed

--- PDGT/PDGT/App.xaml.cs
+++ PDGT/PDGT/App.xaml.cs
@@ -13,7 +13,7 @@ namespace PDGT
 		{
 			InitializeComponent();
 
-			MainPage = new NavigationPage(new SessionPage());
+			MainPage = new NavigationPage(new MainPage());
 		}
 
         protected override void OnStart()
