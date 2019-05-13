debf303a1f92ace86598fb3cc8ea384288ba3a0b
Pia Roel Jorgensen <pia@designersbar.dk>
2019-05-13 10:02:53 +0200

    Feature: ExerciseActivePage
    Created Page for ective exercise. Exercise Guide is now a modal opening from button at the bottom.

---
+++ PDGT/PDGT/Pages/ExerciseActivePage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PDGT.Pages
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class ExerciseActivePage : ContentPage
+	{
+		public ExerciseActivePage ()
+		{
+			InitializeComponent ();
+		}
+
+        private async void GuideBtn_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushModalAsync(new Viewguide());
+
+        }
+    }
+}
