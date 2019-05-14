58713fb0ecf0ee608474425560de7ad07a5f0a12
Pia Roel Jorgensen <pia@designersbar.dk>
2019-05-14 12:12:39 +0200

    Feature: Add animated purple circle

--- PDGT/PDGT/Pages/ExerciseActivePage.xaml.cs
+++ PDGT/PDGT/Pages/ExerciseActivePage.xaml.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Windows;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -12,15 +12,31 @@ namespace PDGT.Pages
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class ExerciseActivePage : ContentPage
 	{
-		public ExerciseActivePage ()
+        
+        public ExerciseActivePage ()
 		{
 			InitializeComponent ();
-		}
+
+                      
+            async void Animate(BoxView SetsDone)
+            {
+                do
+                {
+                    await Task.WhenAny<bool>
+                       ( 
+                        SetsDone.ScaleTo(1, 1000)
+                        );
+                    await SetsDone.ScaleTo(1.2, 1000);
+                } while (true);
+            }
+
+            Animate(SetsDone);                                       
+        }
+               
 
         private async void GuideBtn_Clicked(object sender, EventArgs e)
         {
             await Navigation.PushModalAsync(new Viewguide());
-
         }
 
         private async void FinishSetBtn_Clicked(object sender, EventArgs e)
